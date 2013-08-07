383d91f07df66151f4019f1bc84ab666c0467979
John Walk <johnrwalk@gmail.com>
2013-08-06 22:45:06 -0400

    continued work with eqdsk reader

--- eqdskReader.py
+++ eqdskReader.py
@@ -2,7 +2,6 @@
 
 from EqTools import *
 import scipy
-import os
 import glob
 
 class EQDSKReader(Equilibrium):
@@ -28,12 +27,16 @@ class EQDSKReader(Equilibrium):
         #instantiate superclass, forcing time splining to false (eqdsk only contains single time slice)
         super(EQDSKReader,self).__init__(length_unit=length_unit,tspline=False)
 
+        self._shot = shot
+        self._time = scipy.array([time])
+
         #parse shot and time inputs into standard naming convention
         if len(str(time)) < 5:
             timestring = '0'*(5-len(str(time))) + str(time)
         elif len(str(time)) > 5:
             timestring = str(time)[-5:]
-            print('Time window string greater than 5 digits.  Masking to last 5 digits.  If this violates the selected EQ files, \
+            print('Time window string greater than 5 digits.  Masking to last 5 digits.  \
+                  If this does not match the selected EQ files, \
                   please use explicit filename inputs.')
         else:   #exactly five digits
             timestring = str(time)
@@ -42,27 +45,48 @@ class EQDSKReader(Equilibrium):
 
         #if explicit filename for g-file is not set, check current directory for files matching name
         if gfilename is None:
+            print('Searching directory for file g'+name+'.')
             gcurrfiles = glob.glob('g'+name+'*')
             if len(gcurrfiles) == 1:
                 gfilename = gcurrfiles[0]
+                print('File found.')
             elif len(gcurrfiles) > 1:
-                raise ValueError('multiple valid g-files detected in directory.  Please select a file with explicit \
+                raise ValueError('Multiple valid g-files detected in directory.  \
+                                  Please select a file with explicit \
                                   input or clean directory.')
             else:   #no files found
-                raise ValueError('no valid g-files detected in directory.  Please select a file with explicit input or \
+                raise ValueError('No valid g-files detected in directory.  \
+                                  Please select a file with explicit input or \
                                   ensure file is in directory.')
+        else:   #check that given file is in directory
+            gcurrfiles = glob.glob(gfilename)
+            if len(gcurrfiles) < 1:
+                raise ValueError('No g-file with the given name detected in directory.  \
+                                  Please ensure the file is in the active directory or \
+                                  that you have supplied the correct name.')
 
         #and likewise for a-file name
         if afilename is None:
+            print('Searching directory for file a'+name+'.')
             acurrfiles = glob.glob('a'+name+'*')
             if len(acurrfiles) == 1:
                 afilename = acurrfiles[0]
+                print('File found.')
             elif len(acurrfiles) > 1:
-                raise ValueError('multiple valid a-files detected in directory.  Please select a file with explicit \
+                raise ValueError('Multiple valid a-files detected in directory.  \
+                                  Please select a file with explicit \
                                   input or clean directory.')
             else:   #no files found
-                raise ValueError('no valid a-files detected in directory.  Please select a file with explicit input or \
+                raise ValueError('No valid a-files detected in directory.  \
+                                  Please select a file with explicit input or \
                                   ensure file in in directory.')
+        else:   #check that given file is in directory
+            acurrfiles = glob.glob(afilename)
+            if len(acurrfiles) < 1:
+                raise ValueError('No a-file with the given name detected in directory.  \
+                                  Please ensure the file is in the active directory or \
+                                  that you have supplied the correct name.')
+                
         
 
 
