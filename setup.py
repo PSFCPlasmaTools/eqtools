a2c214c78161f0938ed06d3524fc4a4c06590f1a
faustian <icfaust@gmail.com>
2013-08-30 03:31:08 -0400

    needed modifications to insure setup works

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from numpy.distutils.core import setup,Extension
 
-tricub = Extension('_tricub',['_tricub.pyf','_tricub.c'])
+tricub = Extension('eqtools._tricub',['eqtools/_tricub.pyf','eqtools/_tricub.c'])
 
-pysetup(name='eqtools',
+setup(name='eqtools',
         version='1.0',
         description='Tokamak Flux mapping utility',
         author=['Mark Chilenski','Ian Faust','John Walk'],
         author_email='tbd@psfc.mit.edu',
         url='https://github.com/PSFCPlasmaTools/EqTools/',
-        packages=['eqtools',]
+        packages=['eqtools',],
         ext_modules = [tricub,]
         )  
