1aed2e182c99895fbb8980fadc68ea37cc394bc2
faustian <icfaust@gmail.com>
2014-06-04 18:09:04 -0400

    modified splines with a try catch to work on 0d signals

--- eqtools/core.py
+++ eqtools/core.py
@@ -3291,7 +3291,7 @@ class Equilibrium(object):
             Rout = self.getRmidOut(length_unit='m')[time_idxs]
         else:
             magR = self.getMagRSpline(length_unit='m')(time_idxs)
-            Rout = self._getRmidOutSpline(length_unit='m')(time_idxs)
+            Rout = self.getRmidOutSpline(length_unit='m')(time_idxs)
         
         # Compute r/a according to our definition:
         return (R_mid - magR) / (Rout - magR)
@@ -3326,7 +3326,7 @@ class Equilibrium(object):
             Rout = self.getRmidOut(length_unit='m')[time_idxs]
         else:
             magR = self.getMagRSpline(length_unit='m')(time_idxs)
-            Rout = self._getRmidOutSpline(length_unit='m')(time_idxs)
+            Rout = self.getRmidOutSpline(length_unit='m')(time_idxs)
         
         # Compute R_mid according to our definition:
         return roa * (Rout - magR) + Rout
@@ -4717,11 +4717,20 @@ class Equilibrium(object):
             if kind == 'nearest' and self._tricubic:
                 kind = 'cubic'
 
-            self._magRSpline = scipy.interpolate.interp1d(self.getTimeBase(),
-                                                          self.getMagR(length_unit=length_unit),
-                                                          kind=kind,
-                                                          bounds_error=False)
-
+            try:
+                self._magRSpline = scipy.interpolate.interp1d(self.getTimeBase(),
+                                                              self.getMagR(length_unit=length_unit),
+                                                              kind=kind,
+                                                              bounds_error=False)
+            except ValueError:
+                # created to allow for single time (such as gfiles) to properly call this method
+                kind = 'zero'
+                fill_value = self.getMagR(length_unit=length_unit)
+                self._magRSpline = scipy.interpolate.interp1d([0.],[0.],
+                                                                   kind=kind,
+                                                                   bounds_error=False,
+                                                                   fill_value=fill_value)
+                
             return self._magRSpline
 
     def getMagZSpline(self, length_unit=1, kind='nearest'):
@@ -4764,10 +4773,21 @@ class Equilibrium(object):
             if kind == 'nearest' and self._tricubic:
                 kind = 'cubic'
 
-            self._magZSpline = scipy.interpolate.interp1d(self.getTimeBase(),
-                                                          self.getMagZ(length_unit=length_unit),
-                                                          kind=kind,
-                                                          bounds_error=False)
+            try:
+                self._magZSpline = scipy.interpolate.interp1d(self.getTimeBase(),
+                                                              self.getMagZ(length_unit=length_unit),
+                                                              kind=kind,
+                                                              bounds_error=False)
+
+            except ValueError:
+                # created to allow for single time (such as gfiles) to properly call this method
+                kind = 'zero'
+                fill_value = self.getMagZ(length_unit=length_unit)
+                self._magRSpline = scipy.interpolate.interp1d([0.],[0.],
+                                                              kind=kind,
+                                                              bounds_error=False,
+                                                              fill_value=fill_value)
+                
 
             return self._magZSpline
     
@@ -4811,56 +4831,24 @@ class Equilibrium(object):
             if kind == 'nearest' and self._tricubic:
                 kind = 'cubic'
 
-            self._RmidOutSpline = scipy.interpolate.interp1d(self.getTimeBase(),
-                                                             self.getRmidOut(length_unit=length_unit),
-                                                             kind=kind,
-                                                             bounds_error=False)
+            try:
+                self._RmidOutSpline = scipy.interpolate.interp1d(self.getTimeBase(),
+                                                                 self.getRmidOut(length_unit=length_unit),
+                                                                 kind=kind,
+                                                                 bounds_error=False)
+            except ValueError:
+                # created to allow for single time (such as gfiles) to properly call this method
+                kind = 'zero'
+                fill_value = self.getRmidOut(length_unit=length_unit)
+                self._magRSpline = scipy.interpolate.interp1d([0.],[0.],
+                                                              kind=kind,
+                                                              bounds_error=False,
+                                                              fill_value=fill_value)
+                
+
 
             return self._RmidOutSpline
     
-
-    def _getRmidOutSpline(self, length_unit=1, kind='cubic'):
-        """Gets the univariate spline to interpolate R_out as a function of time.
-        
-        Only used if the instance was created with keyword tspline=True.
-        
-        Keyword Args:
-            length_unit: String or 1. Length unit that R_out is returned in. If
-                a string is given, it must be a valid unit specifier:
-                    'm'         meters
-                    'cm'        centimeters
-                    'mm'        millimeters
-                    'in'        inches
-                    'ft'        feet
-                    'yd'        yards
-                    'smoot'     smoots
-                    'cubit'     cubits
-                    'hand'      hands
-                    'default'   meters
-                If length_unit is 1 or None, meters are assumed. The default
-                value is 1 (R_out returned in meters).
-            kind: String or non-negative int. Specifies the type of interpolation
-                to be performed in getting from t to R_out. This is
-                passed to scipy.interpolate.interp1d. Valid options are:
-                'linear', 'nearest', 'zero', 'slinear', 'quadratic', 'cubic'
-                If this keyword is an integer, it specifies the order of spline
-                to use. See the documentation for interp1d for more details.
-                Default value is 'cubic' (3rd order spline interpolation). On
-                some builds of scipy, this can cause problems, in which case
-                you should try 'linear' until you can rebuild your scipy install.
-        
-        Returns:
-            scipy.interpolate.interp1d to convert from t to R_out.
-        """
-        if self._RmidOutSpline:
-            return self._RmidOutSpline
-        else:
-            self._RmidOutSpline = scipy.interpolate.interp1d(self.getTimeBase(),
-                                                             self.getRmidOut(length_unit=length_unit),
-                                                             kind=kind,
-                                                             bounds_error=False)
-            return self._RmidOutSpline
-
     def getInfo(self):
         """
         Abstract method.  See child classes for implementation.
