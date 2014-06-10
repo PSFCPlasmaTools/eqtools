e8732f458cea068f6f8266c554c3db7f97643762
icfaust <icfaust@gmail.com>
2014-06-10 13:48:51 -0400

    added necessary functionality for boundary and limiter

--- eqtools/filewriter.py
+++ eqtools/filewriter.py
@@ -3,9 +3,10 @@ import scipy.interpolate
 import warnings
 import time
 import core
+import matplotlib.pyplot as plt
 #import trispline
 
-def gfile(obj, tin, nw=None, nh=None, shot=None, name=None, tunit = 'ms', title='EQTOOLS'):
+def gfile(obj, tin, nw=None, nh=None, shot=None, name=None, tunit = 'ms', title='EQTOOLS', nbbbs=100):
  
     if shot is None:
         shot = obj._shot
@@ -33,7 +34,7 @@ def gfile(obj, tin, nw=None, nh=None, shot=None, name=None, tunit = 'ms', title=
    
     rgrid = scipy.linspace(obj.getRGrid()[0],obj.getRGrid()[-1],nw)
     zgrid = scipy.linspace(obj.getZGrid()[0],obj.getZGrid()[-1],nh)
-    rgrid,zgrid = scipy.meshgrid(rgrid,zgrid)
+    rgrid2,zgrid2 = scipy.meshgrid(rgrid,zgrid)
     print(header)
 
     gfiler =open(name, 'wb')
@@ -45,10 +46,14 @@ def gfile(obj, tin, nw=None, nh=None, shot=None, name=None, tunit = 'ms', title=
                        obj.getRGrid()[0],
                        obj.getZGrid()[-1]/2.+obj.getZGrid()[0]/2.]))
     
-    gfiler.write(_fmt([obj.getMagRSpline()(tin),
-                       obj.getMagZSpline()(tin),
+    rcent = obj.getMagRSpline()(tin)
+    zcent = obj.getMagZSpline()(tin)
+    psiLCFS = -1*obj.getCurrentSign()*obj._getLCFSPsiSpline()(tin)
+
+    gfiler.write(_fmt([rcent,
+                       zcent,
                        -1*obj.getCurrentSign()*obj._getPsi0Spline()(tin),
-                       -1*obj.getCurrentSign()*obj._getLCFSPsiSpline()(tin),
+                       psiLCFS,
                        0.])) # need to get bzero getter...      
 
     if obj._tricubic:
@@ -64,11 +69,12 @@ def gfile(obj, tin, nw=None, nh=None, shot=None, name=None, tunit = 'ms', title=
     gfiler.write(_fmt([Ip,
                        -1*obj.getCurrentSign()*obj._getPsi0Spline()(tin),
                        0.,
-                       obj.getMagRSpline()(tin),
+                       rcent,
                        0.]))
-    gfiler.write(_fmt([obj.getMagZSpline()(tin),
+
+    gfiler.write(_fmt([zcent,
                        0.,
-                       -1*obj.getCurrentSign()*obj._getLCFSPsiSpline()(tin),
+                       psiLCFS,
                        0.,
                        0.]))
     
@@ -90,10 +96,10 @@ def gfile(obj, tin, nw=None, nh=None, shot=None, name=None, tunit = 'ms', title=
             gfiler.write(_fmt(temp(pts1).ravel()))
 
 
-
-    gfiler.write(_fmt(-1*obj.getCurrentSign()*obj.rz2psi(rgrid,
-                                                         zgrid,
-                                                         tin).ravel())) #spline with new rz grid
+    psiRZ = -1*obj.getCurrentSign()*obj.rz2psi(rgrid2,
+                                               zgrid2,
+                                               tin)
+    gfiler.write(_fmt(psiRZ.ravel())) #spline with new rz grid
 
     if not obj._tricubic:
         temp = scipy.interpolate.interp1d(pts0,
@@ -103,15 +109,103 @@ def gfile(obj, tin, nw=None, nh=None, shot=None, name=None, tunit = 'ms', title=
 
 
         gfiler.write(_fmt(temp(pts1).ravel()))
-        #gfiler.write(fmt([2*
+
+    
+    
+    # find plasma boundary
+    out = findLCFS(rgrid,
+                   zgrid,
+                   psiRZ,
+                   rcent,
+                   zcent,
+                   psiLCFS,
+                   nbbbs=nbbbs)
+
+    #write boundary
+    lim = scipy.array(obj.getMachineCrossSection()).T
+
+    gfiler.write('  '+int(len(out))+'   '+int(len(lim))+'\n')
+
+    gfiler.write(_fmt(out.ravel()))
+    
+    gfiler.write(_fmt(lim.ravel()))
 
     gfiler.close()
-    #        nbbs,limtr #luckily these are easy
-    #        RLCFS,ZCLFS  #this will require some work need to recalc this
-    #        crosssection
-    #else:
-    #    dump
             
+
+def findLCFS(rgrid, zgrid, psiRZ, rcent, zcent, psiLCFS, nbbbs=100):
+    plt.ioff()  
+    fig = plt.figure()
+    cs = plt.contour(rgrid,
+                     zgrid,
+                     psiRZ,
+                     [psiLCFS]) 
+    
+    splines = []
+    for i in cs.collections[0].get_paths():
+        temp = i.vertices
+        # turn points into polar coordinates about the plasma center
+        rvals = scipy.sqrt((temp[:,0] - rcent)**2 + (temp[:,1] - zcent)**2)
+        thetvals = scipy.arctan2(temp[:,1] - zcent,temp[:,0] - rcent)
+       
+        # find all monotonic sections of contour line in r,theta space
+        temp = scipy.diff(thetvals)
+        idx = 0
+        sign = scipy.sign(temp[0])
+        for j in xrange(len(temp)-1):
+            if (scipy.sign(temp[j+1]) != sign): 
+                sign = scipy.sign(temp[j+1])
+                print(thetvals[idx],thetvals[j+1],j+2-idx)
+                #only write data if the jump at the last point is well resolved
+                if (j+2-idx > 2):#abs(thetvals[idx]-thetvals[j+1]) < 7*scipy.pi/4) and 
+                    splines += [scipy.interpolate.interp1d(thetvals[idx:j+2],
+                                                           rvals[idx:j+2],
+                                                           kind='linear',
+                                                           bounds_error=False,
+                                                           fill_value=scipy.inf)]
+                idx = j+1
+ 
+        splines += [scipy.interpolate.interp1d(thetvals[idx:len(thetvals)],
+                                               rvals[idx:len(thetvals)],
+                                               kind='linear',
+                                               bounds_error=False,
+                                               fill_value=scipy.inf)]
+    
+
+    # construct a set of angles about the center, and use the splines
+    # to find the closest part of the contour to the center at that
+    # angle, this is the LCFS, store value. If no value is found, store
+    # an infite value, which is then tossed out.
+
+    outr = scipy.empty((nbbbs,))
+    ang = scipy.linspace(-scipy.pi,scipy.pi,nbbbs)
+    for i in xrange(nbbbs):
+        temp = scipy.inf
+        for j in splines:
+            pos = j(ang[i])
+            if pos < temp:
+                temp = pos
+        outr[i] = temp
+
+    # remove infinites
+    ang = ang[scipy.isfinite(outr)]
+    outr = outr[scipy.isfinite(outr)]
+    
+    #move back to r,z space
+    output = scipy.empty((2,len(ang) + 1))
+    output[0,:-1] = outr*scipy.cos(ang) + rcent
+    output[1,:-1] = outr*scipy.sin(ang) + zcent                   
+    output[0,-1] = output[0,0]
+    output[1,-1] = output[1,0]
+
+    # turn off plotting stuff
+    plt.ion()
+    plt.clf()
+    plt.close(fig)
+    plt.ioff()
+    
+    return output.T
+
     
 def _fmt(val):
     """ data formatter for gfiles, which doesnt follow normal conventions..."""
