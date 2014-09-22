5369d22dd781bf8ddc59809869e17335a32b020b
markchil <mark.chilenski@gmail.com>
2014-09-21 21:36:11 -0400

    Updates to setup.py to get ready for deployment.

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
-from numpy.distutils.core import setup,Extension
-from distutils.core import setup as pysetup
+try:
+    from numpy.distutils.core import setup, Extension
+except ImportError:
+    import warnings
+    warnings.warn("numpy not installed, defaulting to setuptools.")
+    try:
+        from setuptools import setup, Extension
+    except ImportError:
+        warnings.warn("setuptools not installed, defaulting to distutils.")
+        from distutils.core import setup, Extension
+
 tricub = Extension('eqtools._tricub',['eqtools/_tricub.pyf','eqtools/_tricub.c'])
 
-pysetup(name='eqtools',
-        version='1.0',
-        description='Python tools for magnetic equilibria in tokamak plasmas',
-        author=['Mark Chilenski','Ian Faust','John Walk'],
-        author_email='tbd@psfc.mit.edu',
-        url='https://github.com/PSFCPlasmaTools/eqtools/',
-        packages=['eqtools',],
-        )
-setup(ext_modules = [tricub,])
+setup(
+    name='eqtools',
+    version='0.1.0',
+    packages=['eqtools',],
+    install_requires=['scipy', 'numpy', 'matplotlib'],
+    author=['Mark Chilenski','Ian Faust','John Walk'],
+    author_email='tbd@psfc.mit.edu',
+    url='https://github.com/PSFCPlasmaTools/eqtools/',
+    description='Python tools for magnetic equilibria in tokamak plasmas',
+    long_description=open('README.md', 'r').read(),
+    ext_modules = [tricub,],
+    license='GPL'
+)
