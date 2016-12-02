Fixing current sign issues

eqtools/AUGData.py
@@ -1335,7 +1335,7 @@ class AUGDDData(Equilibrium):
             currentSign (Integer): 1 for positive-direction current, -1 for negative.
         """
         if self._currentSign is None:
-            self._currentSign = 1 if scipy.mean(self.getIpMeas()) > 1e5 else -1
+            self._currentSign = -1 if scipy.mean(self.getIpMeas()) > 1e5 else 1
         return self._currentSign
 
 