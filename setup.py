c239a952605e8fdf1b96b88799651d9a87265b8b
markchil <mark.chilenski@gmail.com>
2023-03-11 12:01:44 -0500

    Update version in setup.py.

--- setup.py
+++ setup.py
@@ -13,14 +13,14 @@ tricub = Extension('eqtools._tricub',['eqtools/_tricub.pyf','eqtools/_tricub.c']
 
 setup(
     name='eqtools',
-    version='1.3.1',
+    version='1.4.0',
     packages=['eqtools',],
     install_requires=['scipy', 'numpy', 'matplotlib'],
-    author=['Mark Chilenski','Ian Faust','John Walk'],
+    author=['Mark Chilenski', 'Ian Faust', 'John Walk'],
     author_email='psfcplasmatools@mit.edu',
     url='https://github.com/PSFCPlasmaTools/eqtools/',
     description='Python tools for magnetic equilibria in tokamak plasmas',
     long_description=open('README.md', 'r').read(),
-    ext_modules = [tricub,],
+    ext_modules=[tricub,],
     license='GPL'
 )
