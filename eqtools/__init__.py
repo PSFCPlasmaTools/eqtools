fad958dfda81afd10a2ee683a89159c0fbb06333
markchil <mark.chilenski@gmail.com>
2023-03-11 12:00:46 -0500

    Increment version number to reflect Python 3 support and prep for release.

--- eqtools/__init__.py
+++ eqtools/__init__.py
@@ -19,7 +19,7 @@
 """Provides classes for interacting with magnetic equilibrium data in a variety of formats.
 """
 
-__version__ = '1.3.2_dev'
+__version__ = '1.4.0'
 
 from .core import *
 from .EFIT import *
